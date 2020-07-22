827a64cb8d0e1fc1fdd8585e7c6e1daaec010c16
Ryan Sonshine <sonshine@amazon.com>
2020-07-22 09:00:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# AWS SSO Credentials Helper (WIP)
+
+When using `aws sso login` on AWS CLI v2 the credentials are stored in `~/.aws/cli/cache`.
+This works fine with the CLI itself but is not picked up by other libraries expecting them
+to be in `~/.aws/credentials` (ie: aws-sdk and Terraform).
+
+This script aims to streamline updating the AWS credentials file for AWS SSO users by
+updating/creating the corresponding profile section in `~/.aws/credentials` containing
+the `aws_access_key_id`, `aws_secret_access_key`, and `aws_session_token`.
+
+## Requirements
+
+## Quick Start
+
---
+++ src/types/index.ts
@@ -0,0 +1,56 @@
+import { AccessTokenType, RoleNameType, AccountIdType } from 'aws-sdk/clients/sso';
+
+export interface Profile {
+  output: Output;
+  region: Region;
+  sso_account_id: AccountIdType;
+  sso_region: Region;
+  sso_role_name: RoleNameType;
+  sso_start_url: string;
+}
+
+export interface Credential {
+  region?: Region;
+  aws_access_key_id: string;
+  aws_secret_access_key: string;
+  aws_session_token?: string;
+}
+
+export interface CachedCredential {
+  accessToken: AccessTokenType;
+  expiresAt: string;
+  region: Region;
+  startUrl: string;
+}
+
+export interface ParsedConfig<T> {
+  [key: string]: T;
+}
+
+export type Output = 'json' | 'yaml' | 'text' | 'table';
+export type Region =
+  | 'af-south-1'
+  | 'ap-east-1'
+  | 'ap-northeast-1'
+  | 'ap-northeast-2'
+  | 'ap-northeast-3'
+  | 'ap-south-1'
+  | 'ap-southeast-1'
+  | 'ap-southeast-2'
+  | 'ca-central-1'
+  | 'cn-north-1'
+  | 'cn-northwest-1'
+  | 'eu-central-1'
+  | 'eu-north-1'
+  | 'eu-south-1'
+  | 'eu-west-1'
+  | 'eu-west-2'
+  | 'eu-west-3'
+  | 'me-south-1'
+  | 'sa-east-1'
+  | 'us-east-1'
+  | 'us-east-2'
+  | 'us-gov-east-1'
+  | 'us-gov-west-1'
+  | 'us-west-1'
+  | 'us-west-2';
