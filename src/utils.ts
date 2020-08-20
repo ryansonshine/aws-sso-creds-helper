e8bf852f9601c9e41e712c6c96657302cb90155a
Ryan Sonshine <sonshine@amazon.com>
2020-08-20 16:25:18 -0400

    style(prettier): change print width back to 80

--- src/utils.ts
+++ src/utils.ts
@@ -10,7 +10,9 @@ export const writeConfig = <T>(filename: string, config: T): void => {
 };
 
 export const readConfig = <T>(filename: string): ParsedConfig<T> => {
-  const config = parse(readFileSync(filename).toString('utf-8')) as ParsedConfig<T>;
+  const config = parse(
+    readFileSync(filename).toString('utf-8')
+  ) as ParsedConfig<T>;
   return config;
 };
 
@@ -36,7 +38,10 @@ export const loadJson = (path: string): unknown => {
   }
 };
 
-export const isMatchingStartUrl = (cred: CachedCredential, profile: Profile): boolean => {
+export const isMatchingStartUrl = (
+  cred: CachedCredential,
+  profile: Profile
+): boolean => {
   return cred.startUrl === profile?.sso_start_url;
 };
 
@@ -49,7 +54,8 @@ export const isCredential = (
   config: Profile | CachedCredential | unknown
 ): config is CachedCredential => {
   return Boolean(
-    (config as CachedCredential)?.accessToken && (config as CachedCredential).expiresAt
+    (config as CachedCredential)?.accessToken &&
+      (config as CachedCredential).expiresAt
   );
 };
 
