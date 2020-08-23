b9481e30338b83944e34230e7ebecdfc01659d26
Ryan Sonshine <sonshine@amazon.com>
2020-08-23 14:46:14 -0400

    test(jest): add coverage collection paths to jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.spec.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/__tests__/**/*.ts',
+    '!<rootDir>/src/types/**/*.ts',
+  ],
   globals: {
     'ts-jest': {
       diagnostics: false,
